37ae260160e9b0944e0efb337841500d94461b1b
Dimo Mitev <dimosoftuni@gmail.com>
2025-01-16 21:35:54 +0200

    add funny message2

--- person.cs
+++ person.cs
@@ -6,7 +6,7 @@ class Person
     public string Name { get; set; }
     public int Age { get; set; }
 
-    // Constructor
+    // Funny Constructor
     public Person(string name, int age)
     {
         Name = name;
